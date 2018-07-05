86a50614a13f83500ed85b95eb72d49f29bd48f4
Aron <aronfreyrh@gmail.com>
2018-07-05 15:51:07 +0000

    Minor refactoring

--- notesfromxml/views.py
+++ notesfromxml/views.py
@@ -60,7 +60,7 @@ def display_docs(request):
             doc = Document.objects.get(document_name=doc_name)
             handle_new_tag(tag, doc)
 
-    return render(request, 'notesfromxml/displaydocs.html',
+    return render(request, 'notesfromxml/display-all-docs.html',
                   {'documents': documents, 'form': AddTagForm()})
 
 
@@ -100,7 +100,7 @@ def display_docs_with_tags(request):
                 tag_object = Tag.objects.get(tag_name=tag)
                 docs_with_tag = Document.objects.filter(tagmap__tag=tag_object)
                 list_of_docs_with_tags.extend(docs_with_tag)
-    return render(request, 'notesfromxml/doc_by_tag.html', {'documents': list_of_docs_with_tags})
+    return render(request, 'notesfromxml/doc_by_tag.html', {'documents': list_of_docs_with_tags, 'form': AddTagForm()})
 
 
 def edit_doc(request, doc):
@@ -177,30 +177,4 @@ def display_all_pages(request):
 
 # A test view that displays the stuff behind the "Test" button in the navigation bar.
 def display_tests(request):
-    test_text = """
-        <p>Lets Spring auto-wire other beans into your classes using <code>@Autowired</code> annotation. 
-        Spring beans can be wired by name or by type.</p> 
-        <ul>
-        <li>
-        <code>@Autowire</code> by default is a type driven injection. <code>@Qualifier</code> spring annotation can be used to further fine-tune autowiring.
-        </li>
-        <li>
-        <code>@Resource</code> (javax.annotation.Resource) annotation can be used for wiring by name.
-        </li>
-        </ul>
-        <p>Beans that are themselves defined as a collection or map type cannot be injected through @Autowired, because type matching is not properly applicable to them.
-        Use @Resource for such beans, referring to the specific collection or map bean by unique name.  @Autowired allows you to do a single:</p>
-        <pre><code class="language-java" data-lang="java">@Autowired
-public NBIServiceImpl nbiServicePort;</code></pre>
-        
-        <p>Instead of constantly doing:</p>
-        
-        <pre><code class="language-java" data-lang="java">NBIServiceImpl nbiServiceImpl = new NBIServiceImpl(nbiServicePort, env);</code></pre>
-        
-        <p>Whenever you want to make a new NBIServiceImpl object.
-        For this to work, the NBIServiceImpl class must have a constructor like so:</p>
-        <pre><code class="language-java" data-lang="java">@Inject     public NBIServiceImpl(NBIServicePort client, Environment env) {         this.client = client;         this.env = env;     }</code></pre>
-        
-        <p>Why <code>@Inject</code>? I don't know. Some say that there is no difference between <code>@Autowired</code> and <code>@Inject</code> but that has yet to be confirmed.</p>
-        """
-    return render(request, 'notesfromxml/tests.html', {'test_text': test_text})
+    return render(request, 'notesfromxml/tests.html')
