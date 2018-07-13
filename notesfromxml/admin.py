ad26322ab838e58cc516a9a6e61c000b8e80cd0b
aronh <aronh@siminn.is>
2018-07-13 14:56:10 +0000

    Changed the database a little and added rudimentary image support.

--- notesfromxml/admin.py
+++ notesfromxml/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
-from .models import Document, Tag, Tagmap
+from .models import Document, Tag, Tagmap, Image, ImageDocumentMap, ImageTagMap
 
 
 admin.site.register(Document)
 admin.site.register(Tag)
 admin.site.register(Tagmap)
+admin.site.register(Image)
+admin.site.register(ImageDocumentMap)
+admin.site.register(ImageTagMap)
--- notesfromxml/models.py
+++ notesfromxml/models.py
@@ -28,4 +28,41 @@ class Tagmap(models.Model):
     )
 
     def __str__(self):
-        return 'doc: ' + self.document.document_name + " - tag: " + self.tag.tag_name
+        return 'doc: ' + self.document.document_name + ' - tag: ' + self.tag.tag_name
+
+
+class Image(models.Model):
+    image_name = models.TextField(blank=True)
+    image_text = models.TextField(blank=True)
+    image_picture = models.ImageField(upload_to='gallery')
+
+    def __str__(self):
+        return self.image_name
+
+
+class ImageTagMap(models.Model):
+    tag = models.ForeignKey(
+        'Tag',
+        on_delete=models.CASCADE
+    )
+    image = models.ForeignKey(
+        'Image',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return 'image: ' + self.image.image_name + ' - tag: ' + self.tag.tag_name
+
+
+class ImageDocumentMap(models.Model):
+    document = models.ForeignKey(
+        'Document',
+        on_delete=models.CASCADE
+    )
+    image = models.ForeignKey(
+        'Image',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return 'image: ' + self.image.image_name + ' - document: ' + self.document.document_name
