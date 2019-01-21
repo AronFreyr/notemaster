41eb26903c3b7947187366321034097ba05a95d4
aronh <aronh@siminn.is>
2019-01-21 14:27:29 +0000

    * Minor tests for documents-to-xml conversion.

--- notesfromxml/tests/xml_converter_tests.py
+++ notesfromxml/tests/xml_converter_tests.py
@@ -1,9 +1,51 @@
 from django.test.testcases import TestCase
 from ..models import Document, Tag, Tagmap
 import turtle
+import xml.etree.ElementTree
 
 
 class DocumentMapperTests(TestCase):
+
+    def setUp(self):
+        Document.objects.create(document_name="Java", document_text="Java document text test 1")
+        Document.objects.create(document_name="JAXB", document_text="JAXB document text test 1")
+        Document.objects.create(document_name="XML", document_text="XML document text test 1")
+
+        Tag.objects.create(tag_name="Java")
+        Tag.objects.create(tag_name="JAXB")
+        Tag.objects.create(tag_name="XML")
+
+        Tagmap.objects.create(document=Document.objects.get(document_name="Java"), tag=Tag.objects.get(tag_name="Java"))
+        Tagmap.objects.create(document=Document.objects.get(document_name="JAXB"), tag=Tag.objects.get(tag_name="Java"))
+        Tagmap.objects.create(document=Document.objects.get(document_name="JAXB"), tag=Tag.objects.get(tag_name="JAXB"))
+        Tagmap.objects.create(document=Document.objects.get(document_name="JAXB"), tag=Tag.objects.get(tag_name="XML"))
+        Tagmap.objects.create(document=Document.objects.get(document_name="XML"), tag=Tag.objects.get(tag_name="XML"))
+
+    def test_setup(self):
+        """
+        Test to see if it is possible to create documents, tags, tagmaps and other objects
+        in the "setUp" and reference them in test cases without using "self".
+        :return:
+        """
+        test_doc = Document.objects.get(document_name='JAXB')
+        print(test_doc.document_name)
+
+    def test_create_xml_from_document(self):
+        test_doc = Document.objects.get(document_name='JAXB')
+        xml_text = ''
+        xml_text += '<document>\n'
+        xml_text += '<name>' + test_doc.document_name + '</name>\n'
+        xml_text += '<text>' + test_doc.document_text + '</text>\n'
+        xml_text += '<document_tags>\n'
+        for tag in test_doc.get_all_tags_sorted():
+            xml_text += '<document_tag>' + tag.tag_name + '</document_tag>\n'
+        xml_text += '</document_tags>\n'
+        xml_text += '</document>'
+        print(xml_text)
+        xml_element = xml.etree.ElementTree.fromstring(xml_text)
+        print(xml_element.tag)
+        for child in xml_element:
+            print(child.tag, child.text)
 
     def test_get_document_connections(self):
         Document.objects.create(document_name="Java", document_text="Java document text test 1")
