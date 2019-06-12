c2e147ab1b9e6e072b60762f469bdebd7e03191e
aronh <aronh@siminn.is>
2019-06-12 16:56:21 +0000

    * some progress on XML converting.

--- notesfromxml/services/xml_converter.py
+++ notesfromxml/services/xml_converter.py
@@ -19,11 +19,14 @@ def test_create_xml_from_documents():
         xml_text += '<text> <![CDATA[' + input_text + ']]> </text>\n'
         xml_text += '<document_tags>\n'
         for tag in doc.get_all_tags_sorted():
-            xml_text += '<document_tag>' + tag.tag_name + '</document_tag>\n'
+            xml_text += '<document_tag>\n'
+            xml_text += '<tag_name>' + tag.tag_name + '</tag_name>\n'
+            xml_text += '<tag_type>' + tag.tag_type + '</tag_type>\n'
+            xml_text += '<meta_tag_type>' + tag.meta_tag_type + '</meta_tag_type>\n'
+            xml_text += '</document_tag>\n'
         xml_text += '</document_tags>\n'
         xml_text += '</document>\n'
     xml_text += '</documents>'
-
 
     xml_element = LET.ElementTree(objectify.fromstring(xml_text))
 
@@ -35,15 +38,15 @@ def test_create_xml_from_documents():
 
     with open('./notesfromxml/tests/all_docs_as_xml_test1.xml', 'r') as xml_file:
         xml_text = xml_file.read()
-        print(xml_text)
+        #print(xml_text)
         xml_text = xml_text.replace("<?xml version='1.0' encoding='UTF-8'?>", '')
         new_xml_element = LET.ElementTree(objectify.fromstring(xml_text))
-        root = new_xml_element.getroot()
-        #print(new_xml_element)
-        for child in root:
-            print(child.tag)
-            print(child[0].tag)
-            for child0 in child:
-                print(child0.tag)
-        #print(xml_file.read())
-        #LET.ElementTree(objectify)
+        for elem in new_xml_element.findall('./document'):
+            print('--------------')
+            print('NAME:', elem.name)
+            print('TEXT:', elem['text'])
+            for elem2 in elem.findall('./document_tags/document_tag'):
+                print('TAG:', elem2.tag_name)
+                print('TYPE:', elem2.tag_type)
+                print('META_TYPE:', elem2.meta_tag_type)
+            print('---------------')
