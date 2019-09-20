9d1c7363fe5403960c256e6ec8330daf4a646f5c
aronh <aronh@siminn.is>
2019-09-20 15:45:39 +0000

    * Some minor start in implementing django channels to the project.

--- notemaster/settings.py
+++ notemaster/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
     'notesfromxml',
 ]
 
@@ -69,6 +70,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'notemaster.wsgi.application'
+
+ASGI_APPLICATION = 'notemaster.routing.application'
 
 
 # Database
