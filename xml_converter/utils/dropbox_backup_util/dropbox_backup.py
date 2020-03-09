087707552d888607b9c92fe3e90bc4621a725860
Aron <aronfreyrh@gmail.com>
2020-03-09 23:09:47 +0000

    Codifying the way that the dropbox uploader works.

---
+++ xml_converter/utils/dropbox_backup_util/dropbox_backup.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import os
+import datetime
+
+
+def download_xml():
+    """ Gets the newest version of the xml for documents and tags and uploads it to my dropbox folder. """
+    d = datetime.datetime.today()
+    current_date = d.strftime('%Y-%m-%d')
+    this_path = Path(os.path.dirname(os.path.realpath(__file__)))
+    uploader_path = Path(this_path / 'Dropbox-Uploader' / 'dropbox_uploader.sh')
+    xml_path = Path(this_path / 'downloaded_xml/')
+    xml_doc_path = Path(xml_path / 'documents.xml')
+
+    #os.system('curl -o documents.xml einsk.is:8080/notemaster/xml/documents/')
+
+    os.system('curl -o ' + str(xml_doc_path) + ' einsk.is:8080/notemaster/xml/documents/')
+    #os.system('/home/aron/Dropbox-Uploader/dropbox_uploader.sh upload /home/aron/dropbox_backup_test/documents.xml notemaster_backups/documents_' + current_date + '.xml')
+    upload_string = str(uploader_path) + ' upload ' + str(xml_doc_path.as_posix()) + ' notemaster_backups/documents_' + current_date + '.xml.test'
+    print('upload_string:', upload_string)
+    os.system(upload_string)
+
+    #os.system('curl -o tags.xml einsk.is:8080/notemaster/xml/tags/')
+    #os.system('/home/aron/Dropbox-Uploader/dropbox_uploader.sh upload /home/aron/dropbox_backup_test/tags.xml notemaster_backups/tags_' + current_date + '.xml')
+    xml_tag_path = Path(xml_path / 'tags.xml')
+    os.system('curl -o ' + str(xml_tag_path) + ' einsk.is:8080/notemaster/xml/tags/')
+    tag_upload_string = str(uploader_path) + ' upload ' + str(xml_tag_path.as_posix()) + ' notemaster_backups/tags_' + current_date + '.xml.test'
+    os.system(tag_upload_string)
+
+
+if __name__ == '__main__':
+    download_xml()
---
+++ xml_converter/utils/dropbox_backup_util/run_dropbox_job.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3.6 /home/aron/dropbox_backup_test/test.py
