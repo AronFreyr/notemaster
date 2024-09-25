243ea854c4c82b09b30541c277f7ebf840d2aa43
aronf <aronfreyrh@gmail.com>
2024-09-25 11:31:42 +0200

    Changed the dropbox script that cron runs so that it uses the virtual environment of the project. This stops issues with it not finding the correct python libraries.

--- xml_converter/utils/dropbox_backup_util/run_dropbox_job.sh
+++ xml_converter/utils/dropbox_backup_util/run_dropbox_job.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python3.6 /opt/notemaster/xml_converter/utils/dropbox_backup_util/dropbox_backup.py
+source ../../../venv/bin/activate &&
+python3.6 /opt/notemaster/xml_converter/utils/dropbox_backup_util/dropbox_backup.py &&
+deactivate
