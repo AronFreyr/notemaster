6d700a4ab2e92f20fb06550ac20b48bd2d34da1c
aronf <aronfreyrh@gmail.com>
2025-01-19 21:21:53 +0100

    You can now see tags with the diary entries. You can now only create one diary entry for each day. You can now see a list of all entries created.

--- logbook/urls.py
+++ logbook/urls.py
@@ -6,5 +6,6 @@ app_name = 'logbook'
 urlpatterns = [
     path('', views.index, name='index'),
     path('create_entry/', views.create_entry, name='create_entry'),
+    path('display_all_entries/', views.display_all_entries, name='display_all_entries'),
     path('display_entry/<int:entry_id>/', views.display_entry, name='display_entry'),
 ]
--- logbook/views.py
+++ logbook/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect, reverse
 
 from .forms import CreateDiaryEntryForm
 from .models import DiaryEntry
+from notes.services.object_handling import handle_new_tag
 
 # Create your views here.
 
 
 def index(request):
 
-    all_entries = DiaryEntry.objects.all()
+    all_entries = DiaryEntry.objects.all().order_by('-entry_date')
 
     return render(request, 'logbook/index.html',
                   {'all_entries': all_entries})
@@ -21,13 +22,30 @@ def display_entry(request, entry_id):
                   {'entry': entry})
 
 
+def display_all_entries(request):
+    all_entries = DiaryEntry.objects.all().order_by('-entry_date')
+
+    return render(request, 'logbook/display-all-entries.html',
+                  {'all_entries': all_entries})
+
+
 def create_entry(request):
     if request.method == 'POST':
         create_diary_entry_form = CreateDiaryEntryForm(request.POST)
         if create_diary_entry_form.is_valid():
+            entry_date = create_diary_entry_form.cleaned_data['entry_date']
+            if DiaryEntry.objects.filter(entry_date=entry_date).exists():
+                return render(request, 'logbook/create-diary-entry.html',
+                              {'create_diary_entry_form': create_diary_entry_form,
+                               'error_message': 'An entry for this date already exists. Please choose another date.'})
             new_diary_entry = create_diary_entry_form.save(commit=False)
+            new_diary_entry.document_name = (f'Diary Entry {new_diary_entry.entry_date}'
+                                             f'-{request.user}-{request.user.id}')
             new_diary_entry.document_created_by = request.user
             new_diary_entry.save()
+            diary_tags = create_diary_entry_form.cleaned_data['new_tag']
+            if diary_tags:
+                handle_new_tag(new_tags=diary_tags, new_doc=new_diary_entry, tag_creator=request.user)
             return redirect(reverse('logbook:index'))
 
     return render(request, 'logbook/create-diary-entry.html',
